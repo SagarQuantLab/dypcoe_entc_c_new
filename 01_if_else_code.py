bcfebbaa89e97a302e1d8a8a39e698e513cda308
LAPTOP-JREMQ6PU\HP <s06.patil@gmail.com>
2026-03-23 09:38:27 +0530

    added if else code

---
+++ 01_if_else_code.py
@@ -0,0 +1,19 @@
+# if
+my_age = 19
+if my_age > 18:
+    print(f"I am adult, because my age is : {my_age}")
+
+# if else
+if my_age > 18:
+    print("I am adult")
+elif my_age == 18:
+    print("Turing adult")
+else:
+    print("I am minor")
+
+# reducing if code block
+age_status = False
+if isinstance(my_age, int):
+    age_status = True
+    
+print(age_status)
