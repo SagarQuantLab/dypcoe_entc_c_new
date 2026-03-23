86c223b53c13e5b1d01acfcc3870895f78c2bacb
LAPTOP-JREMQ6PU\HP <s06.patil@gmail.com>
2026-03-23 10:54:11 +0530

    break and continue statement

---
+++ 03_continue_break_statement.py
@@ -0,0 +1,11 @@
+# # continue statement
+# for i in range(6):
+#     if i == 4:
+#         continue
+#     print(i)
+
+# break statement
+for i in range(6):
+    if i == 4:
+        break
+    print(i)
