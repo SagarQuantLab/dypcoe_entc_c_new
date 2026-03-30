4ec88c12e29855b5bc3aa918c252f2da63f2ba45
LAPTOP-JREMQ6PU\HP <s06.patil@gmail.com>
2026-03-30 10:21:43 +0530

    added simple class

--- 11_classes.py
+++ 11_classes.py
@@ -10,6 +10,14 @@ class myCar:
     def __init__(self, name, maker):
         self.name = name
         self.maker = maker
+        self.greetFunction()
 
-carInstance = myCar('Maruti', 'Baleno')
-print(carInstance.maker, carInstance.name)
+    def greetFunction(self):
+        return f"Hello {self.name} from {self.maker}"
+    
+    def car_details(self, cc):
+        return f"Hello this is {self.name} from {self.maker} which has a capacity of {cc}"
+
+carInstance = myCar("Baleno", "Maruti")
+print(carInstance.greetFunction())
+print(carInstance.car_details('1500cc'))
